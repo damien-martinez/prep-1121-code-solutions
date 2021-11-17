4b761dbde25be2af53a69ded114cafa187c7d2c1
Damien Martinez <mart.damien1@gmail.com>
2021-11-16 18:06:45 -0800

    prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,19 @@
+var person = {
+  firstName: 'Damien',
+  lastName: 'Martinez',
+  hobby: 'jelly beans'
+};
+
+console.log(person);
+
+var fullName = person.firstName + ' ' + person.lastName;
+
+console.log(fullName);
+
+person.job = 'Recruiter';
+console.log(person.job);
+
+person.previousJob = 'Analyst';
+console.log(person.previousJob);
+
+console.log(person);
